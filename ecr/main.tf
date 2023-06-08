5ad63ad817098d74339454e6da78b008609bd57c
sachalau <sacha.lau@gmail.com>
2023-06-08 12:13:36 +0200

    allowing force delete for ECR

--- ecr/main.tf
+++ ecr/main.tf
@@ -2,6 +2,7 @@ resource "aws_ecr_repository" "default" {
   for_each             = var.ecr_app_repo_names
   name                 = "${var.project_name}-${each.value}"
   image_tag_mutability = var.ecr_image_tag_mutability
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
