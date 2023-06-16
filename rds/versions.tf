7168722d0c64d9b7098cc684b56488265c3ab842
sachalau <sacha.lau@gmail.com>
2023-06-16 15:53:59 +0200

    managed password needs aws provider 4.61 and up

--- rds/versions.tf
+++ rds/versions.tf
@@ -1,10 +1,11 @@
 terraform {
   required_version = ">= 1.0"
 
+# Requiring 4.61 and higher for managed password option implementation
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.45"
+      version = ">= 4.61"
     }
 
     random = {
