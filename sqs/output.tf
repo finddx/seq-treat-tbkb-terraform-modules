ccea39adedab86a1328d980a0e518aca84f77945
sachalau <sacha.lau@gmail.com>
2023-09-25 10:33:20 +0200

    output queue url

--- sqs/output.tf
+++ sqs/output.tf
@@ -1,9 +1,15 @@
 output "sqs_queue_arn" {
   description = "The arn off SQS queue."
-  value      = aws_sqs_queue.queue.arn
+  value       = aws_sqs_queue.queue.arn
 }
 
 output "sqs_queue_name" {
   description = "The arn off SQS queue."
-  value      = aws_sqs_queue.queue.name
+  value       = aws_sqs_queue.queue.name
+}
+
+
+output "sqs_queue_url" {
+  description = "The URL of SQS queue."
+  value       = aws_sqs_queue.queue.url
 }
