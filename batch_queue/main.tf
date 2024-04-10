da234d55fd6e2e5c7ebcd0c41a6217a9a0f7e4ff
sachalau <sacha.lau@gmail.com>
2024-04-10 17:03:08 +0200

    updating deprecated option for batch queue

--- batch_queue/main.tf
+++ batch_queue/main.tf
@@ -2,9 +2,10 @@ resource "aws_batch_job_queue" "this" {
   name     = var.queue_name
   state    = "ENABLED"
   priority = 1
-  compute_environments = [
-    var.compute_environments
-  ]
+  compute_environment_order {
+    order               = 1
+    compute_environment = var.compute_environments
+  }
   tags = {
     Name = var.queue_name
   }
