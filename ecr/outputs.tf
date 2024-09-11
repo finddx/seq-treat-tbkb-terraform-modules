00aa7e9f67f37c2d8b467b73e48765a509d5bb9c
sachalau <sacha.lau@gmail.com>
2024-09-11 08:40:34 +0200

    getting container repos arn out

--- ecr/outputs.tf
+++ ecr/outputs.tf
@@ -1,4 +1,9 @@
 output "ecr_repositories" {
-  value      = try({for k, v in aws_ecr_repository.default : k => v.repository_url})
+  value      = try({ for k, v in aws_ecr_repository.default : k => v.repository_url })
+  depends_on = [aws_ecr_repository.default]
+}
+
+output "ecr_arns" {
+  value      = try({ for k, v in aws_ecr_repository.default : k => v.arn })
   depends_on = [aws_ecr_repository.default]
 }
