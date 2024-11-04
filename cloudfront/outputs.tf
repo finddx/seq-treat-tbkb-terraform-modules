d4a56f1e3e6f5f23e3ab163323f8f75d6e25a336
Sacha Laurent <sacha.laurent@finddx.org>
2024-11-04 16:51:09 +0100

    changing logic for bucket naming, getting arns out of the module

--- cloudfront/outputs.tf
+++ cloudfront/outputs.tf
@@ -6,7 +6,18 @@ output "distribution_arn" {
   value = aws_cloudfront_distribution.this.arn
 }
 
-
 output "distribution_domain" {
   value = aws_cloudfront_distribution.this.domain_name
+}
+
+output "django-static-bucket" {
+  value = aws_s3_bucket.django_static.arn
+}
+
+output "static-bucket" {
+  value = aws_s3_bucket.static.arn
+}
+
+output "cf-logs" {
+  value = aws_s3_bucket.logs.arn
 }
